e73ad66aba997db57c0936b522f8d2363b7934db
Kostiantyn Madiar <>
2018-12-26 21:39:06 +0200

    auto layout adjustments

--- Atlas/Modules/RegionList/RegionsListController.swift
+++ Atlas/Modules/RegionList/RegionsListController.swift
@@ -26,6 +26,14 @@ final class RegionsListController: ATRootViewController, RegionsListView {
         }
     }
     
+    var itemsGroup = [RegionsListItem]() {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView?.reloadData()
+            }
+        }
+    }
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -36,38 +44,87 @@ final class RegionsListController: ATRootViewController, RegionsListView {
 }
 
 extension RegionsListController: UITableViewDelegate, UITableViewDataSource {
-    
     func refreshData() {
-        if let source = dataService {
-            items = source.getRegions(completion: { [weak self] result in
-                guard let strongSelf = self else { return }
-                switch result {
-                case .success(let regions):
-                    strongSelf.items = regions.map({ RegionsListItem(name: $0.title(), code: $0.rawValue, group: false) })
-                case .failure(let error):
-                    strongSelf.alert.show(errorMessage: error.customDescription)
-                }
-            }).map({ RegionsListItem(name: $0.title(), code: $0.rawValue, group: false) })
+        guard let source = dataService else {
+            return
         }
+        items = source.getRegions(completion: { [weak self] result in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .success(let regions):
+                strongSelf.items = regions.map({ RegionsListItem(name: $0.title(), code: $0.rawValue, group: false) })
+            case .failure(let error):
+                strongSelf.alert.show(errorMessage: error.customDescription)
+            }
+        }).map({ RegionsListItem(name: $0.title(), code: $0.rawValue, group: false) })
+        
+        itemsGroup = source.getRegionGroups(completion: { [weak self] result in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .success(let regionsGroup):
+                strongSelf.itemsGroup = regionsGroup.map({ RegionsListItem(name: $0.title(), code: $0.rawValue, group: true) })
+            case .failure(let error):
+                strongSelf.alert.show(errorMessage: error.customDescription)
+            }
+        }).map({ RegionsListItem(name: $0.title(), code: $0.rawValue, group: false) })
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        var sections = 0
+        sections += items.count > 0 ? 1 : 0
+        sections += itemsGroup.count > 0 ? 1 : 0
+        return sections
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        if section == 0 {
+            return items.count
+        }
+        
+        return itemsGroup.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let item = items[(indexPath as NSIndexPath).row]
+        let section = (indexPath as NSIndexPath).section
+        let item: RegionsListItem
+        if section == 0 {
+            item = items[(indexPath as NSIndexPath).row]
+        } else {
+            item = itemsGroup[(indexPath as NSIndexPath).row]
+        }
+        
         cell.textLabel?.text = item.name
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        onItemSelect?(items[(indexPath as NSIndexPath).row])
+        let section = (indexPath as NSIndexPath).section
+        let item: RegionsListItem
+        if section == 0 {
+            item = items[(indexPath as NSIndexPath).row]
+        } else {
+            item = itemsGroup[(indexPath as NSIndexPath).row]
+        }
+        onItemSelect?(item)
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: 200, height: 50))
+        label.text = section == 0 ? "Regions" : "Region Groups"
+        label.textColor = .black
+        view.addSubview(label)
+//        view.atlas_pinAllEdgesOfSubview(subview: label)
+        
+        
+        
+        return view
     }
 }
